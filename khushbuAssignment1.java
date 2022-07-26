ba795845fedf90d342d1fc3fad2c67ff0b9a301a
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2022-07-26 11:27:37 +0530

    Create khushbuAssignment1.java

---
+++ khushbuAssignment1.java
@@ -0,0 +1,98 @@
+
+// question 1
+//class Hobby {
+//	public static void main(String arg[]) {
+/*		Hobby obj = new Hobby();
+		obj.nameAndAge("Khushbu", 20);
+	}
+	void nameAndAge(String a, int b) { 
+	System.out.println("name : " + a + " " + "age :" + b);
+		
+	}
+	
+}
+*/
+ // question 2 :
+  /* example names according to standard naming convension
+  *  i) Class:- First letter of the name for the class should be Capital and the rest alphabets should be small
+  *		example: CalciWorking , Hobby.
+  *	ii) Function name : Every function should have their identifiers's first letter small and if there are multiple words , is should be named in camel case form,
+  *		example: nameAndAge() , method(). 
+  *		note : function name should always be followed by round brackets '()' ;
+  *	iii) variable:- Variables will always be written as first letter small (similar to functions) but the difference is that variables do not use '()' brackets at the end of their identifiers.
+  *			example: myName , my_name.
+  iv) Static variable: Static variable will always be named in all caps seperated by underscore
+  *			example : MY_STATIC_VARIABLE, STATIC_VAR
+  */
+  
+  /* question 3
+	i) ans: .java file is the file which contains the source code 
+	this file cannot be executed
+	---> .class file is the executable file of .java file
+			after the compilation of .java file, the .class file is formed.
+	ii) Java is not purely OOP'as programming language because of primitive data types.
+	peimitive data types need not to be formed with the help of classes and objects.
+	althought concept of wrapper classes also exists,
+	iii) Example of NR-WA method
+		void add2Integers (int a, int b) {
+			int total = a + b;
+			System.out.println("prints additon of 2 integers : " total);
+		}
+  */
+  
+  
+  
+  
+  
+   //Question 4
+  
+  
+  /* class CalciWorking {
+	  public static void main(String arg[]) {
+		  Calci obj = new Calci();
+		  obj.addy(1,2);
+		  obj.subby(4,2);
+		  obj.diviji(8,2);
+		  obj.mull(6,2);
+		  
+	  }
+  }
+  class Calci {
+	  static void addy(int a,int b) { 
+	 
+	  System.out.println("addy() called in Class CalciWorking" +"add 2 int ="+" " + a+b);
+		  
+	  } 
+	  static void subby(int c, int e) {
+		   int sub= c - e;
+	  System.out.println("subby() called in Class CalciWorking" +"subtracts 2 int ="+" " +sub );
+		
+		  
+	  }
+	  void diviji(int g , int h) {
+		  int div = g/h;
+		  System.out.println("diviji() called in Class CalciWorking" + "divides 2 int ="+" " + div );
+		  
+	  }
+	    void mull(int x, int y) {
+			int mul= x*y;
+			System.out.println("mull() called in Class CalciWorking"+"multiply 2 int ="+" " + mul);
+		  
+	  }
+	  
+	  
+  }
+ */ 
+  
+ 
+ 
+ 
+  
+  /* Question 5 : 
+  Wrapper classes are the classes of primitive data types by which primitaive data tives wille be defined with the help of class ad objects.
+  for example: Integer i = 20; where Integer is the class which contains integer type objects and i is the object of classs containg value i.
+  when we define a primitive data into its corresponding wrapper class it is know as autoboxing 
+  opposite of Autoboxing is known as AutoUnboxing.
+  */
+  
+ 
