af0cdcccce4f9efde4a68334b3961bd8e91d091a
Khushbu Koul <91137719+khushbu2002@users.noreply.github.com>
2022-07-28 22:54:37 +0530

    task 1 week 2, SwitchProg

---
+++ SwitchProg.java
@@ -0,0 +1,47 @@
+class SwitchProg {
+	//with string array
+	public static void main(String arg[]) {
+		/*String a[] = {"O", "A+", "A", "B+", "B", "C", "D" };
+			for (int i=0; i<a.length ; i++) {
+				switch(i) {
+					case 0: System.out.println(" if marks are between 90-100 then grade is = " + a[i]);
+							break;
+					case 1: System.out.println("if marks are between 80-89 then grade is = " + a[i]);
+							break;
+					case 2: System.out.println("if marks are between 70-79 then grade is = " + a[i]);
+							break;
+					case 3: System.out.println("if marks are between 60-69 then grade is = " + a[i]);
+							break;
+					case 4: System.out.println("if marks are between 50-59 then grade is = " + a[i]);
+							break;
+					case 5: System.out.println("if marks are between 40-49 then grade is = " + a[i]);
+							break;
+					case 6: System.out.println("if marks are between 30-39 then grade is = " + a[i]);
+							break;
+					default: System.out.println("invalid grade");
+				}
+			} */
+		// with no for loop, no array and predefined input
+		 /* String grade = "O";
+		 switch (grade) {
+			 case "O" : System.out.println("marks are between 90-100 ");
+							break;
+			case "A+" : System.out.println("marks are between 80-89 ");
+							break;
+			case "A" :	System.out.println("marks are between 70-79  ");
+							break;
+			case "B+" :	System.out.println("marks are between 60-69 ");
+							break;
+			case "B" :	System.out.println("marks are between 50-59 ");
+							break;
+			case "C" :	System.out.println("marks are between 40-49 ");
+							break;
+			case "D" :	System.out.println("marks are between 30-39  ");
+							break;
+			case "F" :	System.out.println("marks are below  30 then you are fail");
+							break;
+			default : System.out.println("invalid grade");
+							break;
+		 } */
+	}
+}
